91e661891475f684ee1839f08d648cf7803b4c40
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-08-31 20:53:05 +0530

    add files

---
+++ block-BRaaao/code/gizmo/src/Components/Articles.js
@@ -0,0 +1,28 @@
+import database from '../data'
+function Articles(props){
+    return(
+        <section className="articles_sec">
+            <div className="container">
+                <h1 className="heading">Articles</h1>
+                <div className="allarticles flex justify_between flex_wrap">  
+                    {database.map((element) => (
+                        <Card {...element} /> 
+                        // <Card  info={element} /> 
+                        ))}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+function Card(props){
+    return (
+        <article className="eachcard flex_30">
+            {/* <img src="https://static.coindesk.com/wp-content/uploads/2020/05/btc-chart-may-12-1200x628.png" alt="" /> */}
+            <img src={props.urlToImage} alt="" />
+            <h2>{props.title}</h2>
+        </article>
+    )
+}
+
+export default Articles;
---
+++ block-BRaaao/code/gizmo/src/Components/Footer.js
@@ -0,0 +1,17 @@
+function Footer(props){
+    return(
+        <footer>    
+            <div className="container flex justify_between items_center">
+                <h4>2020 All rights reserved</h4>
+                <nav>
+                    <ul className="flex">
+                        <li><i class="fab fa-facebook"></i></li>
+                        <li><i class="fab fa-twitter-square"></i></li>
+                        <li><i class="fab fa-linkedin"></i></li>
+                    </ul>
+                </nav>
+            </div>
+        </footer>
+    )
+}
+export default Footer;
